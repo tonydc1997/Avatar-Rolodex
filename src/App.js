85c026063f40926278063e2d2898cda3ebc310ec
Anthony Curtis <tonydc1997@gmail.com>
2019-09-17 13:07:12 +0200

    style: Format App.js using Prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -9,40 +9,38 @@ class App extends Component {
     this.state = {
       monsters: [
         {
-          name: 'Frankenstein',
+          name: "Frankenstein"
         },
         {
-          name: 'Dracula',
+          name: "Dracula"
         },
         {
-          name: 'Werewolf',
-        },
+          name: "Werewolf"
+        }
       ]
-    }
-
+    };
   }
-    
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      )
-    }
-  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
