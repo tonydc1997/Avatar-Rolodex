2456c5858b0ae3859bb01aedd16c3731e062049c
Anthony Curtis <tonydc1997@gmail.com>
2019-09-17 12:57:57 +0200

    fix: Correct syntax issue

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+
     
+
   }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  }
+    
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      )
+    }
+  
 }
 
 export default App;
