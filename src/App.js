db0377a8eb20a2a7bebc45c426008fe1f1d30984
Anthony Curtis <tonydc1997@gmail.com>
2019-09-18 10:09:18 +0200

    feat: Fetch users when component mounts

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.monsters.map(monster => (
-          <h1 key={monster.id)}> {monster.name} </h1>
+          <h1 key={monster.id}> {monster.name} </h1>
         ))}
       </div>
     );
