3aed84b77ce335f37610635b9af240b5340c014b
Anthony Curtis <tonydc1997@gmail.com>
2019-09-18 10:05:17 +0200

    feat: Update state and return statement
    - Initial state contains empty array of monsters.
    
    - Map over monsters array and return their names

--- src/App.js
+++ src/App.js
@@ -7,26 +7,16 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello World!',
+      monsters: [],
     };
   }
 
   render() {
-    const { string } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{string}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id)}> {monster.name} </h1>
+        ))}
       </div>
     );
   }
