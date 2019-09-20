3f97660f6118a897c03f2a74b131fc9232c96a97
Anthony Curtis <tonydc1997@gmail.com>
2019-09-20 12:06:14 +0200

    feat: Add img to card.component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import './card.styles.css';
 export default function Card(props) {
   return (
     <div className="card-container">
+      <img alt="monsters" src={`https://robohash.org/${props.monster.id}?set=set5`} />
       <h1>{props.monster.name}</h1>
     </div>
   );
