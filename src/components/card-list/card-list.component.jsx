cdd1a1d162434648cd07b972fa10457964f9274d
Anthony Curtis <tonydc1997@gmail.com>
2019-09-20 11:01:59 +0200

    feat: Import /use card-list styles into component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import './card-list.styles.css';
 
 export default function CardList(props) {
-  return <div>{props.children}</div>;
+  return <div className="card-list">{props.children}</div>;
 }
