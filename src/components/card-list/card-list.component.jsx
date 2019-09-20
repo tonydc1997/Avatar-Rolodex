ce2b185ee139ed70d8e93a7c5f4965f4f26bc1fa
Anthony Curtis <tonydc1997@gmail.com>
2019-09-20 11:49:24 +0200

    feat: Pass monster prop from CardList to Card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ export default function CardList(props) {
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
-        <Card key={monster.id} />
+        <Card key={monster.id} monster={monster} />
       ))}
     </div>
   );
