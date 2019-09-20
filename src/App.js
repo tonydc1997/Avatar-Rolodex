ff823b45ca804da7e3e440c568ccb77aeca05d0c
Anthony Curtis <tonydc1997@gmail.com>
2019-09-20 10:53:51 +0200

    feat: Import CardList into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
