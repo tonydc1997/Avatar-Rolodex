b21b1c6509ff8ad48d8262bc5afe4a1c108ab1f5
Anthony Curtis <tonydc1997@gmail.com>
2019-09-20 10:57:10 +0200

    feat: Map over monsters within CardList

--- src/App.js
+++ src/App.js
@@ -20,9 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
