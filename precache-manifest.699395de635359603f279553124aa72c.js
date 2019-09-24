08d128e442a342dc6026adafef607652f8ab40a4
Anthony Curtis <tonydc1997@gmail.com>
2019-09-24 12:55:43 +0200

    Updates

---
+++ precache-manifest.699395de635359603f279553124aa72c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f992d630438732eb0b47ccf32bec821",
+    "url": "/Avatar-Rolodex/index.html"
+  },
+  {
+    "revision": "e7a336ca894f528dc7e6",
+    "url": "/Avatar-Rolodex/static/css/main.e9421e41.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/Avatar-Rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "e7a336ca894f528dc7e6",
+    "url": "/Avatar-Rolodex/static/js/main.a1ef1b3d.chunk.js"
+  },
+  {
+    "revision": "fe096e36867ff2de17f7",
+    "url": "/Avatar-Rolodex/static/js/runtime~main.9c088bf8.js"
+  }
+]);
